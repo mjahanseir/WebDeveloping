53c38cab0bbc41e5133eeecb64bca61a4242d147
mjahanseir <mjahanseir@gmail.com>
2022-03-29 12:58:55 -0600

    Create Database Schema

--- LO1_3_App/server.js
+++ LO1_3_App/server.js
@@ -4,6 +4,20 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 
+
+const db= "mongodb://localhost:27017/musicApp";
+mongoose.connect(DB)
+backend.use(express.json());
+
+const musicSchema= new mongoose.Schema(
+    {
+        album:{type: String,  required:false},
+        artist:{type: String,  required:false},
+        year:{type: number,  required:false},
+        artwork:{type: String,  required:false}
+    }
+)
+
 backend.listen(3010, ()=>{
     console.log("server started")
 })
