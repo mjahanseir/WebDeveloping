de5a9d6c430f7246275003870125905b96d25f6d
mjahanseir <mjahanseir@gmail.com>
2022-03-28 20:55:08 -0600

    First setup of express and running server page

--- LO1_1_IntroNode/app.js
+++ LO1_1_IntroNode/app.js
@@ -4,6 +4,13 @@ const app= express();
 app.get('/', function (req,res){
     res.send("This is the home page!!")
 })
+app.get('/content', function (req,res){
+    res.send("This is the <h1>content</h1> page!!")
+})
+app.get('/content:id', function (req,res){
+    var myVar= req.params.id;
+    res.send(" this is a message with " + myVar + " content")
+})
 app.listen(3010, function (){
     console.log("server started....")
 })
