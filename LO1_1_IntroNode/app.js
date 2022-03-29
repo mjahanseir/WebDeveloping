ffc39952074bade9c42ccac6b6711a846b7e4674
mjahanseir <mjahanseir@gmail.com>
2022-03-28 20:49:08 -0600

    First setup of express and running server page

--- LO1_1_IntroNode/app.js
+++ LO1_1_IntroNode/app.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+const express= require("express");
+const app= express();
+
+app.get('/', function (req,res){
+    res.send("This is the home page!!")
+})
+app.listen(3010, function (){
+    console.log("server started....")
+})
